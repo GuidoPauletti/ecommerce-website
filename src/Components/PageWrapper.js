e4615b7079186812924d698edb14a04ad5b1089a
guidopauletti <guido13pauletti@gmail.com>
2022-05-28 16:08:37 -0300

    tocado de colores

--- src/Components/PageWrapper.js
+++ src/Components/PageWrapper.js
@@ -6,8 +6,8 @@ export const PageWrapper = (props) => (
     <div>
         <header>
             <h1 className="site-heading text-center text-faded d-none d-lg-block">
-                <span className="site-heading-upper text-primary mb-3">A Free Bootstrap Business Theme</span>
-                <span className="site-heading-lower">Business Casual</span>
+                <span className="site-heading-upper text-primary mb-3">Andrea Preisz</span>
+                <span className="site-heading-lower">Bienvenida</span>
             </h1>
         </header>
         
@@ -17,10 +17,10 @@ export const PageWrapper = (props) => (
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto">
-                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/">Home</Link></li>
-                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/about">About</Link></li>
-                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/products">Products</Link></li>
-                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/store">Store</Link></li>
+                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/">Principal</Link></li>
+                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/about">Sobre nosotros</Link></li>
+                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/products">Productos</Link></li>
+                        <li className="nav-item px-lg-4"><Link className="nav-link text-uppercase" to="/store">Tienda</Link></li>
                     </ul>
                 </div>
             </div>
--- src/Components/Pages/Products.js
+++ src/Components/Pages/Products.js
@@ -2,9 +2,9 @@ import React from "react";
 import { SingleProduct } from "../Common/SingleProduct";
 
 const product = [
-    {image:"assets/img/products-01.jpg", title:"Blended to Perfection", subtitle:"Coffees & Teas", description:"We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more."},
-    {image:"assets/img/products-02.jpg", title:"Delicious Treats, Good Eats", subtitle:"Bakery & Kitchen", description:"Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods."},
-    {image:"assets/img/products-03.jpg", title:"From Around the World", subtitle:"Bulk Speciality Blends", description:"Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details."},
+    {image:"assets/img/producto01.jpeg", title:"Hecho a mano", subtitle:"Especial para vos", description:"We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more."},
+    {image:"assets/img/producto02.jpeg", title:"Aritos", subtitle:"A mi estilo", description:"Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods."},
+    {image:"assets/img/producto03.jpeg", title:"Piedritas", subtitle:"Sleccionadas por mi", description:"Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details."},
 ]
 
 export const Products = () => (
