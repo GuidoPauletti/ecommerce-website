ec2d670b242683488569c1a158f83f955f2d24bc
guidopauletti <guido13pauletti@gmail.com>
2022-08-01 21:33:38 -0300

    m

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Footer = () => (
     <footer class="footer text-faded text-center py-5">
-        <div class="container"><p class="m-0 small">Copyright &copy; Your Website 2021</p></div>
+        <div class="container"><p class="m-0 small">Copyright &copy; Andrea Preisz</p></div>
     </footer>
 )
